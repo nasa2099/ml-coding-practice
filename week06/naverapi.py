7ca054e849b156a3f0135a26a88b13dcff2db600
nasa2099 <nazarenoneto13@gmail.com>
2026-04-12 18:45:12 +0900

    naverapi.py

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -1,51 +1,52 @@
 # -*- coding: utf-8 -*-
 import urllib.request
+import urllib.parse
 import datetime
 import json
 
-client_id = 'Client ID'
-client_secret = 'Client Secret'
+client_id = "RU3Rk2lrnSjFBJxJ3KIxkoera"
+client_secret = "nn30rwZDp6"
+
 
 def main():
-
-    node = 'news'                                             # 크롤링할 대상
+    node = 'news'
     srcText = input('검색어를 입력하세요: ')
 
     cnt = 0
     jsonResult = []
 
-    jsonResponse = getNaverSearch(node, srcText, 1, 100)      # [CODE 2]
+    jsonResponse = getNaverSearch(node, srcText, 1, 100)
+
+    if jsonResponse is None:
+        print("Error")
+        return
+
     total = jsonResponse['total']
 
-    while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
+    while ((jsonResponse is not None) and (jsonResponse['display'] != 0)):
         for post in jsonResponse['items']:
             cnt += 1
-            getPostData(post, jsonResult, cnt)                # [CODE 3]
+            getPostData(post, jsonResult, cnt)
 
         start = jsonResponse['start'] + jsonResponse['display']
-        jsonResponse = getNaverSearch(node, srcText, start, 100) # [CODE 2]
+        jsonResponse = getNaverSearch(node, srcText, start, 100)
 
-    print('전체 검색 : %d 건' %total)
+    print('전체 검색 : %d 건' % total)
 
-    with open('%s_naver_%s.json' % (srcText, node), 'w', encoding = 'utf8') as outfile:
-        jsonFile = json.dumps(jsonResult, indent = 4, sort_keys = True,
-                              ensure_ascii = False)
-
+    with open('%s_naver_%s.json' % (srcText, node), 'w', encoding='utf8') as outfile:
+        jsonFile = json.dumps(jsonResult, indent=4, ensure_ascii=False)
         outfile.write(jsonFile)
 
-    print("가져온 데이터 : %d 건" %(cnt))
-    print('%s_naver_%s.json SAVED' % (srcText, node))
 
-
-def getNaverSearch(node, srcText, page_start, display):
+def getNaverSearch(node, srcText, start, display):
     base = "https://openapi.naver.com/v1/search"
     node = "/%s.json" % node
-    parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), page_start, display)
-
+    parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), start, display)
     url = base + node + parameters
-    responseDecode = getRequestUrl(url)               #[CODE 1]
 
-    if (responseDecode == None):
+    responseDecode = getRequestUrl(url)
+
+    if responseDecode is None:
         return None
     else:
         return json.loads(responseDecode)
@@ -54,6 +55,7 @@ def getNaverSearch(node, srcText, page_start, display):
 def getRequestUrl(url):
     req = urllib.request.Request(url)
 
+    # ✅ necessário para funcionar (fix real)
     req.add_header("X-Naver-Client-Id", client_id)
     req.add_header("X-Naver-Client-Secret", client_secret)
 
@@ -62,32 +64,24 @@ def getRequestUrl(url):
         if response.getcode() == 200:
             print("[%s] Url Request Success" % datetime.datetime.now())
             return response.read().decode('utf-8')
+
     except Exception as e:
         print(e)
         print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
         return None
 
 
-def getPostData(post, jsonResult, cnt):  #[CODE 3]
+def getPostData(post, jsonResult, cnt):
     title = post['title']
     description = post['description']
     org_link = post['originallink']
     link = post['link']
 
-    # %a: 짧은 형식의 요일 이름 (예, 'Mon', "Tue', "Wed', ... )
-    # %d: 일 (예, 01, 02, 03, ...)
-    # %b: 짧은 형식의 월 이름  (예, 'Jan', 'Feb', 'Mar', ...)
     pDate = datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S +0900')
     pDate = pDate.strftime('%Y-%m-%d %H:%M:%S')
 
-    jsonResult.append({
-        'cnt': cnt,
-        'title': title,
-        'description': description,
-        'org_link': org_link,
-        'link': link,
-        'pDate': pDate
-    })
-    return
+    jsonResult.append({'cnt': cnt, 'title': title, 'description': description,
+                       'org_link': org_link, 'link': link, 'pDate': pDate})
+
 
 main()
